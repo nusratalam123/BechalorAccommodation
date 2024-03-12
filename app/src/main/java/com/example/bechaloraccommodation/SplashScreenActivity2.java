4c61caeb59585dddd9ecad038209b8fa23764f01
nusratalam123 <nusratalam975@gmail.com>
2024-03-13 00:59:38 +0600

    Initial commit

---
+++ app/src/main/java/com/example/bechaloraccommodation/SplashScreenActivity2.java
@@ -0,0 +1,17 @@
+package com.example.bechaloraccommodation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen2);
+
+    }
+}
